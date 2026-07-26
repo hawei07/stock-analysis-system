7368c0d0a78b5a8691222395eea252f9ca07f280
wusai <wusai@stock-analysis.local>
2026-07-26 15:29:39 +0800

    Restore sankey spacing for income flow

--- static/js/financial_tables.js
+++ static/js/financial_tables.js
@@ -1935,7 +1935,7 @@ async function openIncomeSankey(key) {
       draggable: false,
       nodeAlign: 'justify',
       nodeWidth: 18,
-      nodeGap: 0,
+      nodeGap: 22,
       layoutIterations: 0,
       emphasis: { focus: 'adjacency' },
       label: {
